a52c7df275118ee449ec0d17ab34b044f2dfdd71
Althael <46852680+AlthaelFR@users.noreply.github.com>
2019-03-04 23:20:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
+const token = process.env.TOKEN;
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
@@ -110,4 +110,4 @@ bot.on("message", async message => {
 });
 
 
-bot.login(tokenfile.token);
+bot.login(token);
